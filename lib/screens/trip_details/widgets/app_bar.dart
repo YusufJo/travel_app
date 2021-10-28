393312ee9d1ba36aeab0eaff67a5d443f4a0db1c
AbdulHamid Yusuf <abdulhamidyusuf@AbdulHamids-MacBook-Pro.local>
2021-10-28 04:00:26 +0200

    Refacotr: code format

--- lib/screens/trip_details/widgets/app_bar.dart
+++ lib/screens/trip_details/widgets/app_bar.dart
@@ -2,7 +2,9 @@ part of '../widgets_barrel.dart';
 
 class _AppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+
   const _AppBar({Key? key, required this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
